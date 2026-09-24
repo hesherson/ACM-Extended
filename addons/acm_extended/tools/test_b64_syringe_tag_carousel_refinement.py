51b87e8d19bbc132dc3e3ce3dbaa3cdfcfd7c6b8
mavis <47053238+hesherson@users.noreply.github.com>
2026-09-23 21:20:43 -0700

    Reconcile B64 carousel with instant navigation

--- addons/acm_extended/tools/test_b64_syringe_tag_carousel_refinement.py
+++ addons/acm_extended/tools/test_b64_syringe_tag_carousel_refinement.py
@@ -87,24 +87,18 @@ def test_patient_name_uses_actual_screen_to_head_gap():
     from test_bounded_current_carousel_contract import header_contract
     header_contract()
 
-def test_carousel_motion_is_one_physical_slide_then_zero_duration_rebind():
+def test_carousel_navigation_is_immediate_and_zero_duration():
+    from test_bounded_current_carousel_contract import navigation_contract
+    navigation_contract()
+
+
+def test_single_syringe_does_not_fake_motion():
     move = txt('functions/fn_skCarouselMove.sqf')
-    pick = txt('functions/fn_skCarouselPick.sqf')
-    assert 'private _motion = 0.135;' in move
-    assert '_c ctrlCommit _motion;' in move
-    assert 'private _scale = if (_to>=0' in move
-    assert '[0] call ACME_fnc_skCarouselRender;' in move
-    assert '[_motion] call ACME_fnc_skCarouselRender;' not in move
-    assert '[_dir] call ACME_fnc_skCarouselMove;' in pick
-
-
-def test_single_syringe_still_only_nudges_then_recenters():
-    move = txt('functions/fn_skCarouselMove.sqf')
-    car = txt('functions/fn_skCarouselRender.sqf')
-    assert 'if (_n == 1) exitWith' in move
-    assert 'private _shift=_dir*_rw*0.075;' in move
-    assert '_n == 1 && {_slot != 2}' in car
-
+    from test_bounded_current_carousel_contract import navigation_contract
+    navigation_contract()
+    assert 'if (_n > 1) then {' in move
+    assert '_shift' not in move
+    assert '_motion' not in move
 
 def test_body_map_current_syringe_remains_immediate_administration_source():
     from test_bounded_site_click_handoff import assert_site_contract
