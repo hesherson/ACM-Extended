4f97b10268ee5bacbcc5fcfb9231530be377b4c7
mavis <47053238+hesherson@users.noreply.github.com>
2026-09-24 19:29:03 -0700

    Advance stable 1.2.3 hotfix build to B150

--- addons/acm_extended/tools/test_123_cpr_bvm_chest_access.py
+++ addons/acm_extended/tools/test_123_cpr_bvm_chest_access.py
@@ -18,7 +18,7 @@ def test_123_release_identity_and_hemtt_version():
     assert 'version = "1.2.3";' in acme("config.cpp")
     startup = acme("functions/fn_initForkStartupRuntime.sqf")
     assert 'ACME_infusion_version = "1.2.3";' in startup
-    assert 'ACME_buildBatch = "B149";' in startup
+    assert 'ACME_buildBatch = "B150";' in startup
     assert 'ACME_debugRevision = "";' in startup
     script = raw(ADDONS / "main" / "script_version.hpp")
     for line in ("#define MAJOR 1", "#define MINOR 2", "#define PATCH 3", "#define BUILD 0"):
